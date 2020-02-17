bb39fd367598416272f77721e215889c3afc631f
Simone Dosi <dousi96@hotmail.it>
2020-02-17 19:24:58 +0100

    Single Job to remove holes and triangulate

--- Assets/Scripts/TestEarClippingJob.cs
+++ Assets/Scripts/TestEarClippingJob.cs
@@ -1,11 +1,7 @@
-﻿using System.Collections;
-using UnityEngine;
+﻿using UnityEngine;
 using Unity.Jobs;
 using Unity.Collections;
-
 using dousi96.Geometry.Triangulator;
-
-using JacksonDunstan.NativeCollections;
 
 [RequireComponent(typeof(MeshFilter))]
 [RequireComponent(typeof(MeshRenderer))]
@@ -15,7 +11,6 @@ public class TestEarClippingJob : MonoBehaviour
 
     PolygonJobData polygon;
     NativeArray<int> outTriangles;
-    NativeLinkedList<int> ll;
 
     JobHandle handleTriangulatorJob;
 
@@ -28,7 +23,7 @@ public class TestEarClippingJob : MonoBehaviour
 
     private void TestEarClippingWithJob()
     {
-        Vector2[] contourn1 =
+        Vector2[] contourn =
         {
             new Vector2(-2f, -2f),
             new Vector2(+2f, -2f),
@@ -42,8 +37,8 @@ public class TestEarClippingJob : MonoBehaviour
         {
             new Vector2[]
             {
-                new Vector2(-1f, -1f),
-                new Vector2(-1f, +1f),
+                new Vector2(-1f, -0.75f),
+                new Vector2(-1f, +0.75f),
                 new Vector2(+0f, +1f),
                 new Vector2(-0.5f, 0f),
                 new Vector2(+0f, -1f),
@@ -56,49 +51,28 @@ public class TestEarClippingJob : MonoBehaviour
             },
             new Vector2[]
             {
-                new Vector2(-1.25f, +1f),
+                new Vector2(-1.25f, +0.5f),
                 new Vector2(-1.5f, +0f),
-                new Vector2(-1.25f, -1f),
+                new Vector2(-1.25f, -0.5f),
                 new Vector2(-2.5f, +0f),                
             },
         };
 
-        polygon = new PolygonJobData(contourn1, holes, Allocator.TempJob);
+        PolygonJobData polygon = new PolygonJobData(contourn, holes, Allocator.TempJob);
 
         int totNumVerts = polygon.NumHoles * 2 + polygon.NumTotVertices;
         int ntris = (totNumVerts - 2) * 3;
-        outTriangles = new NativeArray<int>(ntris, Allocator.TempJob);
-        ll = new NativeLinkedList<int>(totNumVerts, Allocator.TempJob);
-
-        //creating the jobs 
-        EarClippingRemoveHolesJob removeHolesJob = new EarClippingRemoveHolesJob()
-        {
-            Polygon = polygon,
-            VertexIndexLinkedList = ll
-        };
+        NativeArray<int> outTriangles = new NativeArray<int>(ntris, Allocator.TempJob);
 
         EarClippingTriangulatorJob triangulatorJob = new EarClippingTriangulatorJob()
         {
             Polygon = polygon,
-            VertexIndexLinkedList = ll,
-            OutTris = outTriangles
+            OutTriangles = outTriangles
         };
 
         //schedule the jobs
-        JobHandle handleRemoveHolesJob = removeHolesJob.Schedule();
-        handleTriangulatorJob = triangulatorJob.Schedule(handleRemoveHolesJob);
+        JobHandle handleTriangulatorJob = triangulatorJob.Schedule();
         handleTriangulatorJob.Complete();
-
-        StartCoroutine(WaitForTriangulationEnd());
-    }
-
-
-    IEnumerator WaitForTriangulationEnd()
-    {
-        while (!handleTriangulatorJob.IsCompleted)
-        {
-            yield return null;
-        }
 
         //get the job results
         Vector3[] vertices = new Vector3[polygon.NumTotVertices];
@@ -115,7 +89,6 @@ public class TestEarClippingJob : MonoBehaviour
 
         polygon.Dispose();
         outTriangles.Dispose();
-        ll.Dispose();
 
         filter.sharedMesh = new Mesh();
         filter.sharedMesh.vertices = vertices;
